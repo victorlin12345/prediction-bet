8fb564d211a99109f6fd3b28f6ec717ff501c3e3
victor <victor@psp-power.com.tw>
2022-12-15 03:22:04 +0800

    refactor

--- contracts/BetGame.sol
+++ contracts/BetGame.sol
@@ -11,6 +11,19 @@ import "hardhat/console.sol";
 // StartTime -> LockTime -> EndTime
 contract BetGame is Ownable, Pausable, ReentrancyGuard {
 
+    event RoundBeginEvent(uint256 indexed roundID, RoundInfo roundInfo);
+    event LockRoundEvent(uint256 indexed roundID, address sender, int256 price, uint32 timestamp);
+    event EndRoundEvent(uint256 indexed roundID, address sender, int256 price, uint32 timestamp);
+    event BetBearEvent(address indexed sender, uint256 indexed roundID, uint256 amount);
+    event BetBullEvent(address indexed sender, uint256 indexed roundID, uint256 amount);
+    event CalculateResultEvent(uint256 indexed roundID, address sender);
+    event ClaimRewardEvent(uint256 indexed roundID, address indexed sender, uint256 amount);
+    event SetIntervalEvent(uint256 interval, uint32 timestamp);
+    event SetIntervalBufferEvent(uint256 buffer, uint32 timestamp);
+    event SetMinBetAmountEvent(uint256 amount, uint32 timestamp);
+    event PauseEvent(uint256 indexed roundID);
+    event UnpausedEvent(uint256 indexed roundID);
+
     modifier notContract() {
         require(!_isContract(msg.sender), "contract not allowed");
         require(msg.sender == tx.origin, "proxy contract not allowed");
@@ -64,10 +77,9 @@ contract BetGame is Ownable, Pausable, ReentrancyGuard {
     }
     
     uint256 currentRoundID = 1;
-    uint256 interval = 60 * 60 * 3; // 3 hours
-    uint256 toleranceInterval = 30;
-    uint256 rewardInterval = 60 * 60 * 1;
-    uint256 minBetAmount = 0.001 ether;
+    uint256 interval = 60 * 60 * 3;       // default: 3 hour
+    uint256 intervalBuffer = 30;          // default: 30 sec
+    uint256 minBetAmount = 0.001 ether;   // default
 
     mapping(uint256 => RoundInfo) public Games;
     mapping(uint256 => mapping(address => BetInfo)) public Bets;
@@ -87,7 +99,7 @@ contract BetGame is Ownable, Pausable, ReentrancyGuard {
         return Bets[_round][_user];
     }
 
-     function GetResultInfo(uint256 _round) public view returns (ResultInfo memory) {
+    function GetResultInfo(uint256 _round) public view returns (ResultInfo memory) {
         return Results[_round];
     }
     
@@ -109,7 +121,7 @@ contract BetGame is Ownable, Pausable, ReentrancyGuard {
 
         RoundInfo storage game = Games[_round];
         require(block.timestamp >= game.LockTime, "should set greater than LockTime");
-        require(block.timestamp <= game.LockTime + toleranceInterval, "should set less than LockTime with tolerance");
+        require(block.timestamp <= game.LockTime + intervalBuffer, "should set less than LockTime with tolerance");
         require(block.timestamp <= game.EndTime, "should set less than EndTime");
         game.LockPrice =  _lockPrice;
     }
@@ -120,7 +132,7 @@ contract BetGame is Ownable, Pausable, ReentrancyGuard {
 
         RoundInfo storage game = Games[_round];
         require(block.timestamp >= game.EndTime, "should set greater than EndTime");
-        require(block.timestamp <= game.EndTime + toleranceInterval, "should set less than EndTime with tolerance");
+        require(block.timestamp <= game.EndTime + intervalBuffer, "should set less than EndTime with tolerance");
         game.EndPrice =  _endPrice;
     }
 
@@ -129,25 +141,28 @@ contract BetGame is Ownable, Pausable, ReentrancyGuard {
         require(price >= 0, "price should greater than 0");
         require(currentRoundID >= 1, "should greater than 1");
         _setRoundBegin(currentRoundID, price);
+        emit RoundBeginEvent(currentRoundID, Games[currentRoundID]);
     }
 
-    function ExecuteRoundLock() public whenNotPaused {
-        require(block.timestamp <= (Games[currentRoundID].LockTime + toleranceInterval), "should execute before lock time with tolerance");
+    function ExecuteRoundLock() public whenNotPaused notContract {
+        require(block.timestamp <= (Games[currentRoundID].LockTime + intervalBuffer), "should execute before lock time with tolerance");
         require(block.timestamp >= Games[currentRoundID].LockTime, "should execute after lock time");
 
         int256 price = GetLatestPrice();
         require(price >= 0, "price should greater than 0");
         _setRoundLock(currentRoundID, price);
+        emit LockRoundEvent(currentRoundID, msg.sender, price, uint32(block.timestamp));
     }
 
-    function ExecuteRoundEnd() public whenNotPaused {
-        require(block.timestamp <= (Games[currentRoundID].EndTime + toleranceInterval), "should execute after end time in tolerance interval");
+    function ExecuteRoundEnd() public whenNotPaused notContract {
+        require(block.timestamp <= (Games[currentRoundID].EndTime + intervalBuffer), "should execute after end time in tolerance interval");
         require(block.timestamp >= Games[currentRoundID].EndTime, "should execute after end time");
 
         int256 price = GetLatestPrice();
         require(price >= 0, "price should greater than 0");
         _setRoundEnd(currentRoundID, price);
         currentRoundID = currentRoundID + 1;
+        emit EndRoundEvent(currentRoundID, msg.sender, price, uint32(block.timestamp));
     }
 
     function GetLatestPrice() public view returns (int256) {
@@ -164,6 +179,7 @@ contract BetGame is Ownable, Pausable, ReentrancyGuard {
         Bets[currentRoundID][msg.sender].RoundID = currentRoundID;
         Bets[currentRoundID][msg.sender].Amount = amount;
         Bets[currentRoundID][msg.sender].Predict = Prediction.Bull;
+        emit BetBullEvent(msg.sender, currentRoundID, amount);
     } 
 
     function BetBear() public payable whenNotPaused nonReentrant notContract bettable {
@@ -175,13 +191,14 @@ contract BetGame is Ownable, Pausable, ReentrancyGuard {
         Bets[currentRoundID][msg.sender].RoundID = currentRoundID;
         Bets[currentRoundID][msg.sender].Amount = amount;
         Bets[currentRoundID][msg.sender].Predict = Prediction.Bear;
+        emit BetBearEvent(msg.sender, currentRoundID, amount);
     }
 
     // 贏家可以拿走自己的 share 以及和輸家全部錢平分給贏家
     function CalculateResult(uint256 _round) public whenNotPaused notContract {
         require(Games[_round].EndPrice > 0, "not yet set up end price");
         require(!Results[_round].BeCalculated, "already be calculated");
-        require(block.timestamp >= Games[_round].EndTime + toleranceInterval * 2, "should call after end time plus twice toleranceInterval");
+        require(block.timestamp >= Games[_round].EndTime + intervalBuffer * 2, "should call after end time with twice buffer time");
         
         uint256 bearParticipants = Games[_round].PredictBearParticipants;
         uint256 bullParticipants = Games[_round].PredictBullParticipants;
@@ -215,24 +232,57 @@ contract BetGame is Ownable, Pausable, ReentrancyGuard {
             require(Results[_round].Share > 0, "share should greater than 0");
         }
 
-        Results[_round].BeCalculated = true;        
+        Results[_round].BeCalculated = true;  
+
+        emit CalculateResultEvent(currentRoundID, msg.sender);      
     }
     
     function ClaimReward(uint256 _round) public whenNotPaused nonReentrant notContract {
-        require(block.timestamp >= Games[_round].EndTime + rewardInterval, "");
         require(Results[_round].BeCalculated, "result should be calculated first");
         require(Bets[_round][msg.sender].Amount > 0, "users amount should greater than 0");
+        require(block.timestamp >= Games[_round].EndTime + intervalBuffer * 2, "should claim reward after end time with twice buffer time");
+        
+        uint256 claimAmount = 0;
+
         // 退費
         if (Results[_round].Answer == Result.NoWinner) {
-             _safeTransfer(msg.sender, Bets[_round][msg.sender].Amount);
+            claimAmount = Bets[_round][msg.sender].Amount;
+             _safeTransfer(msg.sender, claimAmount);
         }
-
+        // 獲勝
         if ((Results[_round].Answer == Result.Bear && Bets[_round][msg.sender].Predict == Prediction.Bear) ||
          (Results[_round].Answer == Result.Bear && Bets[_round][msg.sender].Predict == Prediction.Bear)) {
-            uint256 winAmount = Bets[_round][msg.sender].Amount + Results[_round].Share;
+            claimAmount = Bets[_round][msg.sender].Amount + Results[_round].Share;
             Bets[_round][msg.sender].Amount = 0;
-            _safeTransfer(msg.sender, winAmount);
+            _safeTransfer(msg.sender, claimAmount);
         }
+
+        emit ClaimRewardEvent(currentRoundID, msg.sender, claimAmount);
+    }
+
+    function SetInterval(uint256 _interval) public onlyOwner {
+        interval = _interval;
+        emit SetIntervalEvent(_interval, uint32(block.timestamp));
+    }
+
+    function SetIntervalBuffer(uint256 _intervalBuffer) public onlyOwner {
+        intervalBuffer = _intervalBuffer;
+        emit SetIntervalBufferEvent(_intervalBuffer, uint32(block.timestamp));
+    }
+
+    function SetMinBetAmount(uint256 _amount) public onlyOwner {
+        minBetAmount = _amount;
+        emit SetMinBetAmountEvent(_amount, uint32(block.timestamp));
+    }
+
+    function Pause() public onlyOwner whenNotPaused {
+        _pause();
+        emit PauseEvent(currentRoundID);
+    }
+
+    function Resume() public onlyOwner whenPaused {
+        _unpause();
+        emit UnpausedEvent(currentRoundID);
     }
 
     function _safeTransfer(address to, uint256 value) internal {
